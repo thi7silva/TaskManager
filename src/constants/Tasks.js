75556f21620892657704c1db6ea4838fb965f726
Thiago Antonio <thiago100antonio@gmail.com>
2025-06-14 19:14:31 -0300

    feat: ensure tasks are listed on Tasks component

---
+++ src/constants/Tasks.js
@@ -0,0 +1,46 @@
+const TASKS = [
+  {
+    id: 1,
+    title: 'Estudar FSC',
+    description: 'Estudar para a prova de FSC',
+    time: 'morning',
+    status: 'done',
+  },
+  {
+    id: 2,
+    title: 'Ler livro',
+    description: 'Ler o livro de matemática',
+    time: 'morning',
+    status: 'done',
+  },
+  {
+    id: 3,
+    title: 'Treinar',
+    description: 'Treinar para competição de natação',
+    time: 'afternoon',
+    status: 'in_progress',
+  },
+  {
+    id: 4,
+    title: 'Fazer compras',
+    description: 'Compras os igredientes para o jantar',
+    time: 'afternoon',
+    status: 'in_progress',
+  },
+  {
+    id: 5,
+    title: 'Descansar',
+    description: 'Assistir um filme com a princesa',
+    time: 'evening',
+    status: 'not_started',
+  },
+  {
+    id: 6,
+    title: 'Meditar',
+    description: 'Meditar por 10 minutos',
+    time: 'evening',
+    status: 'not_started',
+  },
+];
+
+export default TASKS;
